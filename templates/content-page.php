e5c7c6d980834ed20df9cb944e9a1654962fe49c
Gianluca Suarato <gianluca.suarato@relatech.com>
2017-03-14 18:27:13 +0100

    fixes

--- templates/content-page.php
+++ templates/content-page.php
@@ -9,12 +9,9 @@
 </header>
 <div class="container">
     <div class="row">
-      <div class="grid-md-6">
+      <div class="grid-md-6 mio-div">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nemo maxime voluptatibus molestiae distinctio fuga quas quaerat neque dolor eligendi non ea cum doloribus quae, quisquam eum ratione nobis officia.</p>
       </div>
-      <div class="grid-md-6">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nemo maxime voluptatibus molestiae distinctio fuga quas quaerat neque dolor eligendi non ea cum doloribus quae, quisquam eum ratione nobis officia.</p>
 
-      </div>
       </div>
       </div>
