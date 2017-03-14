ad3d638906675bcad072e59e6da02c8d5fed0b13
Gianluca Suarato <gianluca.suarato@relatech.com>
2017-03-14 17:37:33 +0100

    added grid logic

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,60 +1,11 @@
 <div class="container">
-    <div class="row text-center">
-            <div class="column col-12">
-              <div class="carousel carousel-autoplay">
-                <input type="radio" id="slide-1" name="carousel-radio" hidden="" class="carousel-locator" checked="">
-              	<input type="radio" id="slide-2" name="carousel-radio" hidden="" class="carousel-locator">
-                <input type="radio" id="slide-3" name="carousel-radio" hidden="" class="carousel-locator">
-              	<input type="radio" id="slide-4" name="carousel-radio" hidden="" class="carousel-locator">
-                <div class="carousel-container">
-                  <figure class="carousel-item">
-                    <label class="item-prev btn btn-action btn-lg" for="slide-4">
-                      <i class="icon icon-keyboard_arrow_left"></i>
-                    </label>
-                    <label class="item-next btn btn-action btn-lg" for="slide-2">
-                      <i class="icon icon-keyboard_arrow_right"></i>
-                    </label>
-                    <img src="https://images.unsplash.com/reserve/wrev1ljvQ6KlfyljCQG0_lion.jpg?dpr=1.25&auto=compress,format&fit=crop&w=1199&h=794&q=80&cs=tinysrgb&crop=" class="img-responsive rounded" alt="macOS Yosemite Wallpaper">
-                  </figure>
+    <div class="row">
+      <div class="grid-md-6">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nemo maxime voluptatibus molestiae distinctio fuga quas quaerat neque dolor eligendi non ea cum doloribus quae, quisquam eum ratione nobis officia.</p>
+      </div>
+      <div class="grid-md-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam nemo maxime voluptatibus molestiae distinctio fuga quas quaerat neque dolor eligendi non ea cum doloribus quae, quisquam eum ratione nobis officia.</p>
 
-                  <figure class="carousel-item">
-                    <label class="item-prev btn btn-action btn-lg" for="slide-1">
-                      <i class="icon icon-keyboard_arrow_left"></i>
-                    </label>
-                    <label class="item-next btn btn-action btn-lg" for="slide-3">
-                      <i class="icon icon-keyboard_arrow_right"></i>
-                    </label>
-                    <img src="img/osx-yosemite-2.jpg" class="img-responsive rounded" alt="macOS Yosemite Wallpaper">
-                  </figure>
-
-                  <figure class="carousel-item">
-                    <label class="item-prev btn btn-action btn-lg" for="slide-2">
-                      <i class="icon icon-keyboard_arrow_left"></i>
-                    </label>
-                    <label class="item-next btn btn-action btn-lg" for="slide-4">
-                      <i class="icon icon-keyboard_arrow_right"></i>
-                    </label>
-                    <img src="img/osx-el-capitan.jpg" class="img-responsive rounded" alt="macOS Yosemite Wallpaper">
-                  </figure>
-
-                  <figure class="carousel-item">
-                    <label class="item-prev btn btn-action btn-lg" for="slide-3">
-                      <i class="icon icon-keyboard_arrow_left"></i>
-                    </label>
-                    <label class="item-next btn btn-action btn-lg" for="slide-1">
-                      <i class="icon icon-keyboard_arrow_right"></i>
-                    </label>
-                    <img src="img/osx-el-capitan-2.jpg" class="img-responsive rounded" alt="macOS Yosemite Wallpaper">
-                  </figure>
-                </div>
-                <div class="carousel-nav">
-                  <label class="nav-item text-hide hand" for="slide-1">1</label>
-                  <label class="nav-item text-hide hand" for="slide-2">2</label>
-                  <label class="nav-item text-hide hand" for="slide-3">3</label>
-                  <label class="nav-item text-hide hand" for="slide-4">4</label>
-                </div>
-              </div>
-            </div>
-          </div>
-
-  </div>
+      </div>
+      </div>
+      </div>
